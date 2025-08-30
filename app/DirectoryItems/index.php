69ba0c7843749439926d17e1337b0797c3e992a1
vanyazaov <vanyazaov@gmail.com>
2025-08-30 20:38:39 +0300

    DirectoryItems: подготовительный этап. Определен класс.

---
+++ app/DirectoryItems/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>DirectoryItems * Images</title>
+</head>
+<body>
+    <h1>DirectoryItems</h1>
+    <p><a href="/">Вернуться назад</a></p>
+</body>
+</html>
---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Examples</title>
+</head>
+<body>
+    <h1>Примеры из книги "Объектно-ориентированное программирование на PHP5" от автора Питер Ловэйн, 2007г.</h1>
+    <ul>
+        <li>Просмотровщик директорий <a href="/DirectoryItems/index.php">DirectoryItems</a></li>
+    </ul>
+</body>
+</html>
