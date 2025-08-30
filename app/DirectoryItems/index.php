7a83978ff9da08d7f6b8e9fa2be5dbf05eb9a545
vanyazaov <vanyazaov@gmail.com>
2025-08-30 21:15:28 +0300

    Финальная версия класса совместимого с PHP4 и демонстрация его работы с выводом галереи изображений из целевой папки.

--- app/DirectoryItems/index.php
+++ app/DirectoryItems/index.php
@@ -8,13 +8,23 @@
     <h1>DirectoryItems</h1>
     <p><a href="/">Вернуться назад</a></p>
     <?php
+        // требуем файл, содержащий определение класса
         require 'DirectoryItems.php';
+        // создаем объект и возвращаем ссылку на неё (совместимость с PHP4)
+        // передаем в конструктор имя каталога
         $di =& new DirectoryItems('graphics');
-        $di->checkAllImages() or die('Не все файлы графические.');
+        // убеждаемся, что все файлы в каталоге - изображения
+        $di->checkAllImages()or die('Не все файлы графические.');
+        // сортируем имена без учета регистра
         $di->naturalCaseInsensitiveOrder();
         // получаем массив
         $filearray = $di->getFileArray();
-        var_dump($filearray);
+        echo "<div style=\"text-align:center;\">";
+        // перебираем массив и выводим собранные данные
+        foreach ($filearray as $value){
+	        echo "<img src=\"graphics/$value\" /><br />file name: $value<br />\n";
+        }
+        echo "</div><br />";
     ?>
 </body>
 </html>
